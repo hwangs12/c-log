f1842097c385a94d4ea491b472d3c6a465ee184f
hwangs12 <sungjun94@gmail.com>
2024-01-17 22:11:25 -0500

    love how to learn

--- 3_rant.c
+++ 3_rant.c
@@ -2,5 +2,19 @@
 
 int main()
 {
-    
+    /* let's not fall for the trap of easy tutorials */
+
+    /* should i still try it? maybe but only if you give all the basics */
+
+    /* so is that a no? */
+
+    /* Correct */
+
+    /* How do I know if I don't take a look at what it is? */
+
+    /* Trust me you'll get there */
+
+    /* No I cannot trust you */
+
+    /* Sure but the more time you invest in it will be less interested in basics */
 }
