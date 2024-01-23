3bfb6d152ab39ae32ad8ad15385bfcfd0118cca1
hwangs12 <sungjun94@gmail.com>
2024-01-22 22:15:33 -0500

    playing

--- 4_howtostartbasics.c
+++ 4_howtostartbasics.c
@@ -4,5 +4,7 @@ int main()
 {
     printf("hey what is this? \c what did you \c with?");
 
+    /* to answer what \c does... it does nothing. I only see c character. could have been done without backslashes. I also got warning from compiler. */
+
     return 0;
 }
