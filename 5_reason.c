d4db594d717cb2c80027c5975c100da372203900
hwangs12 <sungjun94@gmail.com>
2024-01-23 22:17:26 -0500

    hello world c

---
+++ 5_reason.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    /* the main reason starting c now is that */
+    /* as you start learning networking and c++ */
+    /* it all comes down to how the memory is allocated over the message you are transferring over */
+    /* without proper understanding how memory management works in cpu/gpu, it's going to come down to not understanding what the heck is going on */
+}
