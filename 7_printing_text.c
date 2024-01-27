c1b8f6600db2f91ca2d314ecfdd92c3f58a4f2a6
hwangs12 <sungjun94@gmail.com>
2024-01-26 21:30:57 -0500

    hello world

--- 7_printing_text.c
+++ 7_printing_text.c
@@ -1,13 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 int main()
 {
 
     printf("%d, %c, %s \n", 400, 'c', "pay");
     printf("%f \n", 8.9);
     /* you should expect 6 decimals for float */
-    printf("%f \n`", 9);
+    printf("%f \n", 9);
     /* compiler gives warning the type is int so use %d instead */
     /* it gives out 8.90000. so undefined behavior maybe? */
+    printf("%f \n", pow(2, 3));
+    /* doesn't complain for the end value but we get correct decimal value */
+    printf("%f \n", floor(35.444));
     return 0;
 }
