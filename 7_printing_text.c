ce8afeb3fc9d40a4031109bcb26d89b5dd2aa11a
hwangs12 <sungjun94@gmail.com>
2024-01-27 21:25:06 -0500

    log day

--- 7_printing_text.c
+++ 7_printing_text.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 int main()
 {
-    printf("%d, %c, %s", 400, 'c', "pay");
 
+    printf("%d, %c, %s", 400, 'c', "pay");
+    printf("%f", 8.9);
     return 0;
 }
