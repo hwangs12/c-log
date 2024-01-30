9a85c322621d125c8c208369d764dfd8ec0d8170
hwangs12 <sungjun94@gmail.com>
2024-01-29 21:59:39 -0500

    log for the day

--- 9_user_input.c
+++ 9_user_input.c
@@ -2,6 +2,8 @@
 
 int main()
 {
+    /* some codes do not work as expected. How so? */
+
     int age;
     printf("Enter your age: ");
     /* asking which type of data we want from user */
@@ -19,6 +21,13 @@ int main()
     /* no need for ambersand */
     scanf("%s", name);
     printf("Your name is %s \n", name);
+    /* issue with scanf is that everything after scan is ignored */
+    
+    char fullname[20];
+    printf("Enter your full name: ");
+    /* fgets does take space included input no need for ambersand */
+    fgets(fullname, 20, stdin);
+    printf("Your name is %s \n", fullname);
     /* issue with scanf is that everything after scan is ignored */
     return 0;
 }
