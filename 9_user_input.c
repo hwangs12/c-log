1f38ed649e059a58a21da7ea3ae678986954de0c
hwangs12 <sungjun94@gmail.com>
2024-01-29 21:50:42 -0500

    strings input

--- 9_user_input.c
+++ 9_user_input.c
@@ -7,5 +7,17 @@ int main()
     /* asking which type of data we want from user */
     scanf("%d", &age);
     printf("You are %d years old \n", age);
+
+    double gpa;
+    printf("Enter your gpa: ");
+    scanf("%lf", &gpa);
+    /* what is lf? */
+    printf("Your gpa is %lf \n", gpa);
+    
+    char name[20];
+    printf("Enter your name: ");
+    /* no need for ambersand */
+    scanf("%s", name);
+    printf("Your name is %s \n", name);
     return 0;
 }
