11da0ad294480c57f2789273cd60ee8155dd9cb1
hwangs12 <sungjun94@gmail.com>
2024-01-29 21:28:39 -0500

    hello

--- 7_printing_text.c
+++ 7_printing_text.c
@@ -3,8 +3,11 @@
 int main()
 {
 
-    printf("%d, %c, %s", 400, 'c', "pay");
-    printf("%f", 8.9);
+    printf("%d, %c, %s \n", 400, 'c', "pay");
+    printf("%f \n", 8.9);
     /* you should expect 6 decimals for float */
+    printf("%f \n`", 9);
+    /* compiler gives warning the type is int so use %d instead */
+    /* it gives out 8.90000. so undefined behavior maybe? */
     return 0;
 }
