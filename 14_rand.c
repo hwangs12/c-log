0a7720844aaa221366c5ae99be8a876ab5e7dee4
hwangs12 <sungjun94@gmail.com>
2024-02-01 22:14:50 -0500

    srand

--- 14_rand.c
+++ 14_rand.c
@@ -9,4 +9,6 @@ int main(void)
         r = rand() % 100; // Get a random number between 0 and 99
         printf("%d\n", r);
     } while (r != 37);    // Repeat until 37 comes up
+
+    // notice same sequence repeats at each execution
 }
