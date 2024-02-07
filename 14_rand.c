b98d77b1d58e3d5e0189e187a05033c5cccd5f6d
hwangs12 <sungjun94@gmail.com>
2024-02-06 22:19:55 -0500

    some tryouts

--- 14_rand.c
+++ 14_rand.c
@@ -5,10 +5,11 @@ int main(void)
 {
     int r;
 
-    void srand(unsigned int seed);
 
     do {
         r = rand() % 100; // Get a random number between 0 and 99
+        // if you place srand here with r as argument, you will never get value 37
+        srand(r);
         printf("%d\n", r);
     } while (r != 37);    // Repeat until 37 comes up
 
