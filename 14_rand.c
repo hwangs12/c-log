5df87ba373a888fae66c444f4545d4d9e3799c2f
hwangs12 <sungjun94@gmail.com>
2024-02-07 22:22:25 -0500

    close to random

--- 14_rand.c
+++ 14_rand.c
@@ -1,15 +1,17 @@
 #include <stdio.h>   // For printf
 #include <stdlib.h>  // For rand
+#include <time.h>
 
 int main(void)
 {
     int r;
 
+    //this value will change seed every time.
+    srand(time(NULL));
 
     do {
         r = rand() % 100; // Get a random number between 0 and 99
         // if you place srand here with r as argument, you will never get value 37
-        srand(r);
         printf("%d\n", r);
     } while (r != 37);    // Repeat until 37 comes up
 
