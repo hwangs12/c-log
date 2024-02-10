fd3d7245576ad4e73b7eb32f0d88457719c168fa
hwangs12 <sungjun94@gmail.com>
2024-02-10 00:01:59 -0500

    forever loop

--- 15_loop.c
+++ 15_loop.c
@@ -6,4 +6,12 @@ int main()
     for (int i = 0, j = 999; i < 10; i++, j--) {
         printf("%d, %d\n", i, j);
     }
+
+    // empty loop will runs forever
+    for(;;) {  // "forever"
+    printf("I will print this again and again and again\n" );
+    printf("for all eternity until the heat-death of the universe.\n");
+
+    printf("Or until you hit CTRL-C.\n");
+}
 }
