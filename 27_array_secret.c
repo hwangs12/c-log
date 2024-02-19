72ee3059b969923235f509e3607d51c4d4812b8c
hwangs12 <sungjun94@gmail.com>
2024-02-18 23:00:28 -0500

    lovely pointers

--- 27_array_secret.c
+++ 27_array_secret.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int main()
 {
     // pointer to array means pointer to first element of array.
@@ -9,4 +11,9 @@ int main()
     // as a shorthand you can do this.
     p = a; // a little different from actual integer value is assigned because you will use &a not a itself.
     // so you know above is pointer to array.
+
+    int i = 0;
+    int *q = &i;
+
+    printf("%d is the number i pointed at", *q);
 }
