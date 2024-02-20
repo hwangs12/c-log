7c56d0f0b708f81f4d3bfcc150f0372d7a75a173
hwangs12 <sungjun94@gmail.com>
2024-02-19 21:19:43 -0500

    size of string

--- 30_string.c
+++ 30_string.c
@@ -6,12 +6,26 @@ int main()
     char *c = "hello kid!";
     char v[] = "hello vid!";
 
+    char z[12] = "hello vid!";
+
     // pointer type cannot be modified
     // c[0] = 'c';
     v[0] = 'c';
 
     // printf("%s\n", c);
     printf("%s\n", v);
+
+    size_t stringlen = sizeof(v);
+    int leng = stringlen / sizeof(v[0]);
+
+    printf("length of string is %d\n", leng);
+
+    for (int i = 0; v[i]; i++)
+    {
+        printf("%zu \n", sizeof(v[i]));
+    }
+    printf("%zu \n", sizeof(v)); // number of char is inclusive of '\0' terminating char.
+    printf("%zu \n", sizeof(z)); // size of z is size defined in the array size.
 
     return 0;
 }
