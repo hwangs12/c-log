ee580a14425b98f623a4c8cc6ba807aeba0eb33d
hwangs12 <sungjun94@gmail.com>
2024-02-22 21:12:53 -0500

    typedef

---
+++ 38_typedef.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+struct PointType
+{
+    char x;
+    char y;
+};
+
+typedef struct PointType Point; // Setting Point as the alias to PointType
+
+int main()
+{
+    /* without TYPEDEF */
+    struct Point p1;
+    p1.x = 1;
+    p1.y = 3;
+    printf("%d \n", p1.x);
+    printf("%d \n", p1.y);
+
+    /* with TYPEDEF */
+    Point p2; // you don't need struct to indicate PointType. And, you can just use alias.
+    p2.x = 44;
+    p2.y = 100000;
+    printf("%c \n", p2.x);
+    printf("%c \n", p2.y);
+    return 0;
+}
