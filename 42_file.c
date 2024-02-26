ae58117fb650d15ca1e7603b335cda82933071e2
hwangs12 <sungjun94@gmail.com>
2024-02-25 23:19:03 -0500

    reading a text from a file

--- 42_file.c
+++ 42_file.c
@@ -5,3 +5,16 @@
  * stdout   :	Standard Output, generally the screen by default
  * stderr   :	Standard Error, generally the screen by default, as well
  */
+
+int main()
+{
+    /* READING */
+    FILE *fp; // Variable to represent open file
+
+    fp = fopen("hello.txt", "r"); // Open file for reading
+
+    int c = fgetc(fp); // Read a single character
+    printf("%c\n", c); // Print char to stdout
+
+    fclose(fp);
+}
