22393090b39110a541262c26eb5461829fc467bd
hwangs12 <sungjun94@gmail.com>
2024-02-25 23:12:53 -0500

    files

---
+++ 42_file.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/**
+ * stdin    :	Standard Input, generally the keyboard by default
+ * stdout   :	Standard Output, generally the screen by default
+ * stderr   :	Standard Error, generally the screen by default, as well
+ */
