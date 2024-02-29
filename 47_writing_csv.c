5afeded04e4db1d4c23030908f6158984385a5ce
hwangs12 <sungjun94@gmail.com>
2024-02-29 16:27:38 -0500

    writing formatted output with fprintf

--- 47_writing_csv.c
+++ 47_writing_csv.c
@@ -9,7 +9,8 @@ int main(void)
 
     fp = fopen("output.csv", "w");
 
-    printf("%zu", sizeof x / sizeof(int));
+    // fp = stdout; (if you want to write on to a console)
+
     for (int i = 0; i < sizeof(x) / sizeof(int); i++)
     {
         fprintf(fp, "%s, %d, %f\n", z[i], x[i], y[i]); // when reading float, it's one decimal (rounded) but when writing, it's 6 decimals weird!!!
