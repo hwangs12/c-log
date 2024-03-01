1a8b119ca4a490f94715d7c8a5829c7d983e586b
hwangs12 <sungjun94@gmail.com>
2024-03-01 15:14:18 -0500

    anony struct

---
+++ 51_anonymous_struct.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int number_of_branches;
+    int has_leaves;
+} plant; // notice there is no name for the struct just alias
+
+int main()
+{
+    plant coconut_tree;
+    plant pineapple;
+}
