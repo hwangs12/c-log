088d137eac9354764dc02ff6ae2131afd4815ec6
hwangs12 <sungjun94@gmail.com>
2024-03-12 20:39:40 -0400

    math ops with char

---
+++ 81_char_type.c
@@ -0,0 +1,13 @@
+/**
+ * char is also integer (in a small range)
+ * you can do math operations with it too
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    char a = 10, b = 20;
+
+    printf("%d\n", a + b); // 30!
+}
