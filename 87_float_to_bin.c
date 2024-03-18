ff2519943c1ada652aa4a55b18780c44c981caa0
hwangs12 <sungjun94@gmail.com>
2024-03-17 22:37:08 -0400

    steps to convert floating point number to binary format

---
+++ 87_float_to_bin.c
@@ -0,0 +1,9 @@
+/**
+ * step 1: determine sign
+ * step 2: convert the integral portion to unsigned binary
+ * step 3: convert the fractional portion to binary
+ * step 4: normalize the value via adjusting the exponent
+ * step 5: add bias to the exponent
+ * step 6: convert the biased exponent to unsigned binary
+ * step 7: determine the final bits for the mantissa
+ */
