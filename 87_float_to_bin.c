f7445b47b20e0ee062679c96519b87105f3c2120
hwangs12 <sungjun94@gmail.com>
2024-03-18 22:51:39 -0400

    float to bin to ieee

--- 87_float_to_bin.c
+++ 87_float_to_bin.c
@@ -7,3 +7,10 @@
  * step 6: convert the biased exponent to unsigned binary
  * step 7: determine the final bits for the mantissa
  */
+
+/**
+ * IEEE-754
+ * 16 bits - 1 sign bit, 5 exponent bits, 10 fraction bits
+ * 32 bits = 1 sign bit, 8 exponent bits, 23 fraction bits
+ * 64 bits - 1 sign bit, 11 exponent bits, 52 fraction bits.
+ */
