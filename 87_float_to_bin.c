f2e2d6f7eba79f0cf0d2430f99e9d01c869b6a45
hwangs12 <sungjun94@gmail.com>
2024-03-19 20:33:15 -0400

    understanding ieee 754 form

--- 87_float_to_bin.c
+++ 87_float_to_bin.c
@@ -14,3 +14,23 @@
  * 32 bits = 1 sign bit, 8 exponent bits, 23 fraction bits
  * 64 bits - 1 sign bit, 11 exponent bits, 52 fraction bits.
  */
+
+/**
+ * example: -27.15625
+ * conver this to IEEE 754
+ * sign -> negative -> 1
+ * 27 -> 11011
+ * .15625 * 2 = 0.31250 -> 0
+ * 0.31250 * 2 = 0.62500 -> 0
+ * 0.62500 * 2 = 1.25000 -> 1
+ * 0.25 * 2 = 0.5 -> 0
+ * 0.5 * 2 = 1 -> 1
+ *  ---> .00101
+ *
+ * looks like -> 11011.00101
+ * -> 1.101100101 (exponent is 4)
+ *
+ * -> exponent = 15 + 4 = 19
+ * -> in bin -> 10011
+ * -> ieee 754 form: 1100111011001010 (sweet!)
+ */
