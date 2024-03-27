9b128c87392a60efdc3959ed262eb76f5e1a1fd0
hwangs12 <sungjun94@gmail.com>
2024-03-26 21:28:07 -0400

    scientific notation and binary num

---
+++ 91_bin.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 0b101010; // Binary 101010
+
+    printf("%d\n", x); // Prints 42 decimal
+}
---
+++ 92_scientific_notation.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%e\n", 123456.0); // Prints 1.234560e+05
+}
