e09595d93244222200f07ee152419ada9ad82f5a
hwangs12 <sungjun94@gmail.com>
2024-04-09 23:33:12 -0400

    general approach

--- 100_arithmatic_conversion.c
+++ 100_arithmatic_conversion.c
@@ -9,4 +9,6 @@ int main()
     float y = 12 * 2;  // Mixing float and int
                     // 24 is converted to float
                        // 24.0 is stored in y
+
+                       /* Just generally remember that int types become float types if there’s a floating point type anywhere in there, and the compiler makes an effort to make sure mixed integer types don’t overflow. */
 }
