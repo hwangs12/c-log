0c1667260d7a6814e8cc70727651d8df40eed965
hwangs12 <sungjun94@gmail.com>
2024-04-13 00:23:12 -0400

    static

---
+++ 103_static.c
@@ -0,0 +1,12 @@
+/**
+ * has two meanings:
+ * 1. file scope
+ * 2. block scope
+ */
+
+/**
+ * BLOCK SCOPE 
+ * static in block scope means 
+ * single instance of variable shared between calls
+*/
+
