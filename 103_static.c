61996de3e94abe122ee85b397b8a114580f92459
hwangs12 <sungjun94@gmail.com>
2024-04-14 00:25:49 -0400

    static

--- 103_static.c
+++ 103_static.c
@@ -14,3 +14,23 @@
  * static in block scope with an initializer will only be * initialized one time on program startup, not each time * the function is called.
  */
 
+#include <stdio.h>
+
+void counter(void)
+{
+    static int count = 1;  // This is initialized one time
+
+    printf("This has been called %d time(s)\n", count);
+
+    count++;
+}
+
+int main(void)
+{
+    counter();  // "This has been called 1 time(s)"
+    counter();  // "This has been called 2 time(s)"
+    counter();  // "This has been called 3 time(s)"
+    counter();  // "This has been called 4 time(s)"
+    /* notice that count is not assigned to 1 */
+}
+
