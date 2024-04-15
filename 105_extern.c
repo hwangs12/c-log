b6fe690e16370010bcd33d4df774f5898b9ec643
hwangs12 <sungjun94@gmail.com>
2024-04-14 23:18:58 -0400

    how extern works

---
+++ 105_extern.c
@@ -0,0 +1,9 @@
+/* The extern storage-class specifier gives us a way to refer to objects in other source files.
+
+ */
+
+// 105_extern.c
+
+int a = 37;
+
+
---
+++ 106_extern.c
@@ -0,0 +1,13 @@
+// foo.c
+#include <stdio.h>
+
+extern int a;
+
+int main(void)
+{
+    printf("%d\n", a);  // 37, from bar.c!
+
+    a = 99;
+
+    printf("%d\n", a);  // Same "a" from bar.c, but it's now 99
+}
