5c2e70bb0b7e08943108d4b0e040a24e75e51273
hwangs12 <sungjun94@gmail.com>
2024-05-14 21:41:42 -0400

    unnamed bit fields

--- 133_bit_fields.c
+++ 133_bit_fields.c
@@ -9,7 +9,15 @@ struct foo {
     // The tradeoff is, of course, that the 5-bit fields can only hold values from 0-31 and the 3-bit fields can only hold values from 0-7. But life’s all about compromise, after all.
 };
 
+/* unnamed bit fields */
+struct fee {
+    unsigned char a:2;
+    unsigned char :5;   // <-- unnamed bit-field!
+    unsigned char b:1;
+};
+
 int main(void)
 {
     printf("%zu\n", sizeof(struct foo));
+    printf("%zu\n", sizeof(struct fee));
 }
