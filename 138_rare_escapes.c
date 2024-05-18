f7cdb3e08af52060810fb3a437cdb6271d15adf9
hwangs12 <sungjun94@gmail.com>
2024-05-18 13:27:21 -0400

    using some rare escapes

---
+++ 138_rare_escapes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\a %d \v \n", 5);
+    printf("\a aaaaaa\vaaaaaa %d \n", 5);
+    printf("\a aaaaaa\vaaaaaa %d \n", 5);
+    printf("\a aaaaaa\vaaaaaa %d \n", 5);
+
+}
