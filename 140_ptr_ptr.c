51487256f18e8d8ca37be9c4d4defbe8cf85345e
hwangs12 <sungjun94@gmail.com>
2024-05-21 15:21:46 -0400

    storage

--- 140_ptr_ptr.c
+++ 140_ptr_ptr.c
@@ -7,4 +7,10 @@ int main(void)
     int **q = &p;  // Type: pointer to pointer to int
 
     printf("%d %d\n", *p, **q);  // 3490 3490
+
+    printf("---------ADDRESSES-----------\n");
+    printf("value %d stored at %p \n", x, &x);
+    printf("value %p stored at %p \n", p, &p);
+    printf("value %p stored at %p \n", q, &q);
+    printf("---------ADDRESSES-----------");
 }
