405c40999e41a7704b1e763302e3d2fbad113f8d
hwangs12 <sungjun94@gmail.com>
2024-05-23 15:38:52 -0400

    const pointer

--- 141_ptr_const.c
+++ 141_ptr_const.c
@@ -8,4 +8,16 @@ int main(void)
      * 141_ptr_const.c:5:11: warning: initializing 'int **' with an    expression of type 'int *const *' discards qualifiers            [-Wincompatible-pointer-types-discards-qualifiers] int **q = &p;
      * this means that const is discarded
      */
+
+    int x1 = 3490;
+    int *const p1 = &x1;
+    int *const *q1 = &p1;
+
+    /**
+     * line 14 means that whatever q1 points to is const
+    */
+
+    int x2 = 3490;
+    int *const p2 = &x2;
+    int *const *const q2= &p2;  // More const!
 }
