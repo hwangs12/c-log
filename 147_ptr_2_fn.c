6b4254b98eabef28f9c7bd58f8238ad0ae181eb4
hwangs12 <sungjun94@gmail.com>
2024-05-25 21:06:48 -0400

    pointer to fn

---
+++ 147_ptr_2_fn.c
@@ -0,0 +1,7 @@
+/**
+ * Describing a pointer to a function that takes two int 
+ * arguments and returns void 
+ */
+
+float (*p)(int, int); // note you can give the params names, they're just ignored. 
+
---
+++ 148_ptr_2_fn.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* HOW DO WE ASSIGN IT - POINTER TO FUNCTION */
+
+void print_int(int n)
+{
+    printf("%d\n", n);
+}
+
+int main(void)
+{
+    // Assign p to point to print_int:
+
+    void (*p)(int) = print_int;
+
+    p(3490);          // Call print_int via the pointer
+}
