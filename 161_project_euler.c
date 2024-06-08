a6a53b6d38fb53c8b515e27f0ee1a7c8ed04395f
hwangs12 <sungjun94@gmail.com>
2024-06-07 21:30:44 -0400

    setup

---
+++ 161_project_euler.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * A palindromic number reads the same both ways.
+ * The largest palindrome made from the product of two
+ * 2-digit numbers is 9009 = 91 * 99.
+ * 
+ * Find the largest palindrome made from the product of 
+ * two 3-digit numbers.
+*/
+
+/**
+ * let's use the given to our benefit.
+ * we just need to find a pair of 3 digit numbers
+ * that make up palindrome
+ * 
+ * let's build function that checks a number is palindrome
+ * 
+ * then iterate through the 3 digit numbers from high to 
+ * low (999 - 100)
+*/
+
+int isPalindrome(int number)
+{
+    char str[10];
+    sprintf(str, "%d", number);
+    int i = 0;
+    while (i <)
+}
+
+int main()
+{
+
+}
