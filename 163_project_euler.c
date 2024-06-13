7c0c03655a64005538ce59bfa15c2b4b080c1788
hwangs12 <sungjun94@gmail.com>
2024-06-12 23:38:54 -0400

    idea

--- 163_project_euler.c
+++ 163_project_euler.c
@@ -5,7 +5,23 @@
  * even divisible by all of the numbers from 1 to 20?
 */
 
+/**
+ * Given numbers 1 to 10, we can iterate like this:
+ * 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+ * 1, 1, 3, 2, 5, 3, 7, 4, 9, 5 -> taken off 2 from here
+ * 1, 1, 3, 1, 5, 3, 7, 2, 9, 5 -> taken off 2 from here
+ * 1, 1, 3, 1, 5, 3, 7, 1, 9, 5 -> taken off 2 from here
+ * 1, 1, 1, 1, 5, 1, 7, 1, 3, 5 -> taken off 3 from here
+ * 1, 1, 1, 1, 5, 1, 7, 1, 1, 5 -> taken off 3 from here
+ * 1, 1, 1, 1, 1, 1, 7, 1, 1, 1 -> taken off 5 from here
+ * 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 -> taken off 7 from here
+ * 
+ * so the idea is you take each prime factor until you do 
+ * not see them in the list and keep multiplying
+ * those factors until all the members become 1.
+*/
+
 int main()
 {
-    
+
 }
