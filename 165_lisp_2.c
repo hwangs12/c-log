2fe83a487d32191c50a159e13d05a79fa2cd8cdb
hwangs12 <sungjun94@gmail.com>
2024-06-18 22:45:03 -0400

    understanding debugger process

---
+++ 165_lisp_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  puts("Hello, world!");
+  return 0;
+}
+
+// the point of the document is to be able to use 
+// debugger like lldb, gdb, or valgrind
+// will record progress overtime here in the subsequent c docs
