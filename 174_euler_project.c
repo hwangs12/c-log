21be5fd72a212f85c7a4f42c3f14df3963852a9e
hwangs12 <sungjun94@gmail.com>
2024-06-24 20:33:45 -0400

    euler project

---
+++ 174_euler_project.c
@@ -0,0 +1,31 @@
+/**
+ * The four adjacent digits in the 100-digit number
+ * that have the greatest product are 9*9*8*9, 5832
+ * 
+ * Find he thirteen adjacent digits in the 1000-digit
+ * number that have the greatest product. 
+ * 
+ * What is the value of this product?
+ */
+
+/**
+ * it is 1000 digit. Pretty big number to save in
+ * long long int variable. 
+ */
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char arr[1001];
+    printf("please input number here: ");
+    scanf("%s", arr);
+    int i = 0;
+    char currMax[13];
+    
+    while (i < 1000)
+    {
+
+    }
+}
