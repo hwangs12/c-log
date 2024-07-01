a7a45915cc2f2b1bc51f57c489c53d7e9d2bc8a7
hwangs12 <sungjun94@gmail.com>
2024-06-30 21:34:16 -0400

    list of questions to try

---
+++ 175_lisp_7.c
@@ -0,0 +1,24 @@
+/**
+ * Describe textually a grammar for decimal numbers such as
+ * 0.01 or 52.221.
+ */
+
+/**
+ * Describe textually a grammar for web URLs such as 
+ * https://www.buildyourownlisp.com
+ */
+
+/**
+ * Describe textually a grammar for simple English sentences
+ * such as the cat sat on the mat
+ */
+
+/**
+ * Describe more formally the above grammars.
+ * Use |, *, or any symbols of your own invention.
+ */
+
+/**
+ * If you are familiar with JSON,
+ * textually describe a grammar for it.
+ */
