5d47eede2cab1206ab272fb0eb73eb6aee15dd60
hwangs12 <sungjun94@gmail.com>
2024-10-31 22:35:26 -0400

    loop invariant

--- 178_loop_invariants.c
+++ 178_loop_invariants.c
@@ -13,5 +13,13 @@ int find_index(int * arr, int num, int arr_length)
 
 int main()
 {
+    /* what is loop invariant: a logical condition that is true before, during, and after each iteration of a loop in a computer program */
+    /* found index is either null or existing in array */
+    
+    /*  */
 
+    /* loop invariant holds before iteration:  */
+
+    /* loop invariant holds before i = 0 to arr_length-1 */
+    /* loop invariant holds  */
 }
