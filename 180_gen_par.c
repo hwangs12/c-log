293f8804afbddba3283608f5f371736a7dcb2d28
hwangs12 <sungjun94@gmail.com>
2024-11-03 13:08:00 -0500

    problem statement

---
+++ 180_gen_par.c
@@ -0,0 +1,16 @@
+/**
+ * n pair of parentheses,
+ * write function to generate all parentheses
+ * 
+ * n = 3
+ * ((()))
+ * (()())
+ * (())()
+ * ()(())
+ * ()()()
+ */
+
+char** generateParenthesis(int n, int* returnSize)
+{
+    
+}
