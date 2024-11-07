3213207983e9dcac7020f104c29bc91248ec0aa3
hwangs12 <sungjun94@gmail.com>
2024-11-06 22:32:07 -0500

    haha hoho

--- 184.c
+++ 184.c
@@ -9,5 +9,9 @@ int main(void)
 }
 
 /**
- * 
+ * Identify the directives and statements in the program.
+ * What output does the program produce?
+ * Parkinson's Law:
+ * Work expands so as to fill the time
+ * available for its completion. 
  */
