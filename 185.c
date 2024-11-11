4d26e21dae7899f479a1b561e530a6fcae1b55bc
hwangs12 <sungjun94@gmail.com>
2024-11-10 22:48:37 -0500

    dweight

---
+++ 185.c
@@ -0,0 +1,4 @@
+// condense the dweight.c program by replacing the assignments to height, length, and width with initializers and removing the weight variable, instead calculating volumn + 165 / 166 within the last printf
+
+/* dweight.c */
+
