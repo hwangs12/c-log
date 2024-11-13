b674fa47a59888a40aebeab56e3e9a55c477f813
hwangs12 <sungjun94@gmail.com>
2024-11-12 22:53:44 -0500

    condense

--- 185.c
+++ 185.c
@@ -2,3 +2,14 @@
 
 /* dweight.c */
 
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 8, length = 12, width = 10;
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inches): %d\n", height * length * width);
+    printf("Dimensional weight (pounds): %d\n", (height * length * width + 165)/166);
+
+    return 0;
+}
